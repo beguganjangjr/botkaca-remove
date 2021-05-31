8fc637804fcb9caa66b5fca8da737fa3d3008d48
beguganjangjr <64639270+beguganjangjr@users.noreply.github.com>
2021-05-31 22:50:38 +0700

    Update default_message_handler.py

--- bot/handlers/default_message_handler.py
+++ bot/handlers/default_message_handler.py
@@ -3,6 +3,8 @@ from pyrogram.types import Message
 from bot import COMMAND
 from bot.handlers import leech_handler
 import time
+import os
+import tempfile
 
 @Client.on_message(filters.private & ~filters.regex(r'^/') & ~filters.document)
 async def func(client : Client, message : Message):
@@ -11,8 +13,18 @@ async def func(client : Client, message : Message):
 
 @Client.on_message(filters.document & filters.private)
 async def userVideo(client : Client, message : Message):
-    file = message.document
-    link = file.get_file().file_path
-    return await leech_handler.func(client, message)
+    link = None
+    document = message.document
+    if document.file_name.endswith('.torrent'):
+        os.makedirs(str(message.from_user.id), exist_ok=True)
+        fd, link = tempfile.mkstemp(dir=str(message.from_user.id), suffix='.torrent')
+        os.fdopen(fd).close()
+        await message.download(link)
+        mimetype = await get_file_mimetype(link)
+        return await leech_handler.func(client, message)
+    
+        
+
+
     
     
