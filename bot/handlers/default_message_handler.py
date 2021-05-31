7aaee0b68657959b6c16f0b05f2927be1ad43b83
beguganjangjr <64639270+beguganjangjr@users.noreply.github.com>
2021-05-31 22:14:11 +0700

    Update default_message_handler.py

--- bot/handlers/default_message_handler.py
+++ bot/handlers/default_message_handler.py
@@ -4,7 +4,7 @@ from bot import COMMAND
 from bot.handlers import leech_handler
 import time
 
-@Client.on_message(filters.private & ~filters.regex(r'^/'))
+@Client.on_message(filters.private & ~filters.regex(r'^/') & ~filters.document)
 async def func(client : Client, message : Message):
     message.text = "/" + COMMAND.LEECH + " " + message.text
     return await leech_handler.func(client, message)
