ab6afd510c0fa4a2c574b6c6a8a560b84863f569
beguganjangjr <64639270+beguganjangjr@users.noreply.github.com>
2021-05-31 22:10:36 +0700

    Update default_message_handler.py

--- bot/handlers/default_message_handler.py
+++ bot/handlers/default_message_handler.py
@@ -2,8 +2,17 @@ from pyrogram import Client, filters
 from pyrogram.types import Message
 from bot import COMMAND
 from bot.handlers import leech_handler
+import time
 
 @Client.on_message(filters.private & ~filters.regex(r'^/'))
 async def func(client : Client, message : Message):
     message.text = "/" + COMMAND.LEECH + " " + message.text
     return await leech_handler.func(client, message)
+
+@Client.on_message(filters.document & filters.private)
+async def userVideo(client : Client, message : Message):
+    file = message.document
+    link = file.get_file().file_path
+    return await leech_handler.func(client, message)
+    
+    
