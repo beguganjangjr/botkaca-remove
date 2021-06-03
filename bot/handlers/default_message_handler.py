e995bc3add367d16dfec948cf9d606c51bac092a
beguganjangjr <64639270+beguganjangjr@users.noreply.github.com>
2021-06-03 13:18:59 +0700

    Update default_message_handler.py

--- bot/handlers/default_message_handler.py
+++ bot/handlers/default_message_handler.py
@@ -12,16 +12,7 @@ async def func(client : Client, message : Message):
     message.text = "/" + COMMAND.LEECH + " " + message.text
     return await leech_handler.func(client, message)
 
-@Client.on_message(filters.document & filters.private)
-async def userVideo(client : Client, message : Message):
-    link = None
-    document = message.document
-    if document.file_name.endswith('.torrent'):
-        os.makedirs(str(message.from_user.id), exist_ok=True)
-        fd, link = tempfile.mkstemp(dir=str(message.from_user.id), suffix='.torrent')
-        os.fdopen(fd).close()
-        await message.download(link)
-        return await leech_handler.func(client, message)
+
     
         
 
