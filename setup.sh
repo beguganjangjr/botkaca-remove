03a886645f9d970b599d37a9d85ad8861daef3c3
beguganjangjr <64639270+beguganjangjr@users.noreply.github.com>
2021-06-09 13:17:15 +0700

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+wget -q https://kmk.kmk.workers.dev/aria2-1.35.0-static-linux-amd64.tar.gz
+tar xf aria2-1.35.0-static-linux-amd64.tar.gz -C /usr/local/bin
+chmod +x /usr/local/bin/aria2c
+rm -rf aria2-1.35.0-static-linux-amd64.tar.gz
+
+# Create download folder
+#mkdir -p downloads
+
+# DHT
+wget -q https://github.com/P3TERX/aria2.conf/raw/master/dht.dat
+wget -q https://github.com/P3TERX/aria2.conf/raw/master/dht6.dat
