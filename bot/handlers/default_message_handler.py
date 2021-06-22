2835de1f165c8bc9c19bd60bb1f1bedd25f242d2
beguganjangjr <64639270+beguganjangjr@users.noreply.github.com>
2021-06-22 11:31:52 +0700

    Update default_message_handler.py

--- bot/handlers/default_message_handler.py
+++ bot/handlers/default_message_handler.py
@@ -7,10 +7,15 @@ URL_REGEX = r"""(?i)\b((?:https?:(?:/{1,3}|[a-z0-9%])|[a-z0-9.\-]+[.](?:com|net|
 
 @Client.on_message(filters.private & ~filters.regex(r'^/') & ~filters.document | filters.regex(URL_REGEX) & ~filters.regex(r'^/'))
 async def func(client : Client, message : Message):
-    if message.text == message.caption:
-        return
-    message.text = "/" + COMMAND.LEECH + " " + message.text
-    return await leech_handler.func(client, message)
+    if message.caption is not None:
+        try:
+            message.text = "/" + COMMAND.LEECH + " " + message.text
+        except:
+            return
+        return await leech_handler.func(client, message)
+    else:
+        message.text = "/" + COMMAND.LEECH + " " + message.text
+        return await leech_handler.func(client, message)
 
 
 #@Client.on_message(filters.regex(URL_REGEX) & ~filters.regex(r'^/'))
