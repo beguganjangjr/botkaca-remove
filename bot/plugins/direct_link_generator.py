ac923a42f26f8ac866f616a904bdea1ced3dcf84
beguganjangjr <64639270+beguganjangjr@users.noreply.github.com>
2021-06-22 22:48:44 +0700

    Update direct_link_generator.py

--- bot/plugins/direct_link_generator.py
+++ bot/plugins/direct_link_generator.py
@@ -89,7 +89,7 @@ async def direct_link_generator(link: str, proxy):
         or 'dood.so' in link \
         or 'dood.cx' in link \
         or 'dood.to' in link:
-            return dood(link, proxy)
+            return dood(link)
     else:
         raise DirectDownloadLinkException(f'No Direct link function found for {link}')
 
@@ -349,7 +349,7 @@ def doodi(url: str) -> str:
     #return dl_url        
 
 
-def dood(url: str, proxy) -> str:
+def dood(url: str) -> str:
     """ dood direct links generator """
     web_url = re.findall(r'(?://|\.)(dood(?:stream)?\.(?:com|watch|to|so|cx|la))/(?:d|e)/([0-9a-zA-Z]+)', url)[0]
     media_id = web_url[1]
