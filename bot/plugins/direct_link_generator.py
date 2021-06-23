893aeb31839a3f78c421bca354825a8e0d4a8812
beguganjangjr <64639270+beguganjangjr@users.noreply.github.com>
2021-06-23 10:39:03 +0700

    Create direct_link_generator.py

---
+++ bot/plugins/direct_link_generator.py
@@ -0,0 +1,174 @@
+import re
+import aiohttp
+import math, json
+import urllib.parse
+import logging
+import lk21
+from bs4 import BeautifulSoup
+from bot.plugins.exceptions import DirectDownloadLinkException
+from bot.plugins import jsunpack
+from getuseragent import UserAgent
+try:
+    from urllib.parse import urlencode
+except ImportError:
+    from urllib import urlencode
+ua = UserAgent()
+ua = ua.Random()
+LOGGING = logging.getLogger(__name__)
+
+
+
+def get_redirect_url(url, headers={}):
+    request = urllib.request.Request(url, headers=headers)
+    request.get_method = lambda: 'HEAD'
+    response = urllib.request.urlopen(request)
+    return response.geturl()
+
+
+def get_packed_data(html):
+    packed_data = ''
+    for match in re.finditer(r'(eval\s*\(function.*?)</script>', html, re.DOTALL | re.I):
+        if jsunpack.detect(match.group(1)):
+            packed_data += jsunpack.unpack(match.group(1))
+
+    return packed_data
+
+def dood_decode(data):
+    t = string.ascii_letters + string.digits
+    return data + ''.join([random.choice(t) for _ in range(10)])
+
+
+def append_headers(headers):
+    headers = '|%s' % '&'.join(['%s=%s' % (key, urllib.parse.quote_plus(headers[key])) for key in headers])
+    return headers
+
+async def direct_link_generator(url, proxy, session):
+    #blocklinks
+    if 'mega.nz' in url or 'drive.google.com' in url or 'uptobox.com' in url \
+    or '1fiecher.com' in url or 'googleusercontent.com' in url:
+        return "**ERROR:** Unsupported URL!"
+    
+    #mediafire.com
+    elif 'mediafire.com' in url:
+        try:
+            link = re.findall(r'\bhttps?://.*mediafire\.com\S+', url)[0]
+            async with aiohttp.ClientSession() as ttksess:
+                resp = await ttksess.get(link)
+                restext = await resp.text()
+
+            page = BeautifulSoup(restext, 'lxml')
+            info = page.find('a', {'aria-label': 'Download file'})
+            ourl = info.get('href')
+            return ourl
+        except:
+            return "**ERROR:** Cant't download, double check your mediafire link!"
+          
+    #disk.yandex.com    
+    elif 'yadi.sk' in url or 'disk.yandex.com' in url:
+        try:
+            link = re.findall(r'\b(https?://.*(yadi|disk)\.(sk|yandex)*(|com)\S+)', url)[0][0]
+            print(link)
+        except IndexError:
+            return "**ERROR:** Cant't download, double check your yadisk link!"
+
+        api = 'https://cloud-api.yandex.net/v1/disk/public/resources/download?public_key={}'
+        try:
+            async with aiohttp.ClientSession() as ttksess:
+                resp = await ttksess.get(api.format(link))
+                restext = await resp.json()
+                ourl = restext['href']
+                return ourl
+        except:
+            LOGGING.exception("Ayee jooo")
+            return "**ERROR:** Cant't download, the yadisk file not found or dowmload limit reached!" 
+
+    #zippyshare.com
+    elif 'zippyshare.com' in url:
+        try:
+            link = re.findall(r'\bhttps?://.*zippyshare\.com\S+', url)[0]
+            async with aiohttp.ClientSession() as ttksess:
+                resp = await ttksess.get(link)
+                restext = await resp.text()
+            base_url = re.search('http.+.com', restext).group()
+            page_soup = BeautifulSoup(restext, 'lxml')
+            scripts = page_soup.find_all("script", {"type": "text/javascript"})
+            for script in scripts:
+                if "getElementById('dlbutton')" in script.text:
+                    url_raw = re.search(r'= (?P<url>\".+\" \+ (?P<math>\(.+\)) .+);',
+                                        script.text).group('url')
+                    math = re.search(r'= (?P<url>\".+\" \+ (?P<math>\(.+\)) .+);',
+                       script.text).group('math')
+                    url = url_raw.replace(math, '"' + str(eval(math)) + '"')
+                    break
+            ourl = base_url + eval(url)
+            name = urllib.parse.unquote(url.split('/')[-1])
+            return ourl
+        except:
+            return "**ERROR:** Cant't download, double check your zippyshare link!"
+       
+    #racaty.net
+    elif 'racaty.net' in url:
+        try:
+            link = re.findall(r'\bhttps?://.*racaty\.net\S+', url)[0]
+            async with aiohttp.ClientSession() as ttksess:
+                resp = await ttksess.get(link)
+                restext = await resp.text()                      
+            bss=BeautifulSoup(restext,'html.parser')
+            op=bss.find('input',{'name':'op'})['value']
+            id=bss.find('input',{'name':'id'})['value']
+                  
+            async with aiohttp.ClientSession() as ttksess:
+                rep = await ttksess.post(link,data={'op':op,'id':id})
+                reptext = await rep.text()
+            bss2=BeautifulSoup(reptext,'html.parser')
+            ourl = bss2.find('a',{'id':'uniqueExpirylink'})['href']
+            return ourl
+        except:
+            return "**ERROR:** Cant't download, double check your racaty link!"
+       
+    elif 'pixeldrain.com' in url:
+        url = url.strip("/ ")
+        file_id = url.split("/")[-1]
+        
+        info_link = f"https://pixeldrain.com/api/file/{file_id}/info"
+        dl_link = f"https://pixeldrain.com/api/file/{file_id}"
+
+        async with aiohttp.ClientSession() as ttksess:
+            resp = await ttksess.get(info_link)
+            restext = await resp.json()
+
+        if restext["success"]:
+            return dl_link
+        else:
+            return "**ERROR:** Cant't download, {}.".format(restext["value"])
+          
+    elif 'mixdrop.co' in url or 'mixdrop.cx in url':
+      web_url = re.findall(r'(?://|\.)(mixdrop\.(?:co|to|sx))/(?:f|e)/(\w+)', url)[0]
+      media_id = web_url[1]
+      host = web_url[0]
+      link = 'https://' + host + '/e/' + media_id
+      user_agent = ua
+      headers = {'Origin': 'https://{}'.format(host),
+                 'Referer': 'https://{}/'.format(host),
+                 'User-Agent': user_agent}
+      async with session.get(link, headers=headers) as response:
+        d_content = await response.text()
+        
+      r = re.search(r'location\s*=\s*"([^"]+)', d_content)
+      if r:
+        url = 'https://{0}{1}'.format(host, r.group(1))
+        async with session.get(link, headers=headers) as response:
+          d_content = await response.text()
+      if '(p,a,c,k,e,d)' in d_content:
+        d_content = get_packed_data(d_content)
+      r = re.search(r'(?:vsr|wurl|surl)[^=]*=\s*"([^"]+)', d_content)
+      if r:
+        headers = {'User-Agent': user_agent, 'Referer': link}
+        dl_url = "https:" + r.group(1) + append_headers(headers)
+        return dl_url
+      raise DirectDownloadLinkException("`Error: Can't extract the link`\n")        
+      
+    
+    
+      
+          
