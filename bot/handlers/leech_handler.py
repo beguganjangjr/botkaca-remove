b036dfa2770006049b30f283c3ebd4fdeb3f301f
beguganjangjr <64639270+beguganjangjr@users.noreply.github.com>
2021-06-23 19:20:10 +0700

    Update leech_handler.py

--- bot/handlers/leech_handler.py
+++ bot/handlers/leech_handler.py
@@ -151,6 +151,9 @@ async def func(client : Client, message: Message):
              download = await loop.run_in_executor(None, partial(aria2_api.add_uris, [link], options={
                  'continue_downloads' : True,
                  'all-proxy': proxy,
+                 'referer': '*',
+                 'check-certificate': False,
+                 'http-no-cache': True,
                  'out': name}))
              
 
