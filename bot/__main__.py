7d86699f477cb87fe1a30f09a498d9f5581a0a46
beguganjangjr <64639270+beguganjangjr@users.noreply.github.com>
2021-06-27 13:40:46 +0700

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -8,20 +8,28 @@ import asyncio
 import shutil
 import signal
 import pickle
+import traceback
 from os import execl, path, remove
 from . import app
-
+preserved_logs = []
 async def main():
-    fs_utils.start_cleanup()
-    # Check if the bot is restarting
-    if path.exists('restart.pickle'):
-        with open('restart.pickle', 'rb') as status:
-            restart_message = pickle.load(status)
-        #restart_message.edit_text("Restarted Successfully!")
-        #restart_message.edit_text("Restarted Successfully!")
-        LOGGER.info('Restarted Successfully!')
-        remove('restart.pickle')
-        
+    async def _autorestart_worker():
+        while True:
+            try:
+                await app.send_message(1741587120, 'BOT STARTED')
+            except Exception as ex:
+                preserved_logs.append(ex)
+                logging.exception('upload worker commited suicide')
+                tb = traceback.format_exc()
+                for i in STATUS.CHAT_ID:
+                    try:
+                        await app.send_message(i, 'upload worker commited suicide')
+                        await app.send_message(i, tb, parse_mode=None)
+                    except Exception:
+                        logging.exception('failed %s', i)
+                         tb = traceback.format_exc()
+                        
+    asyncio.create_task(_autorestart_worker())   
     app.UPDATES_WORKERS = 100
     app.DOWNLOAD_WORKERS = 100
     app.set_parse_mode("html")
