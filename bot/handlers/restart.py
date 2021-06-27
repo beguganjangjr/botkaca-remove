d536c7b5f37290d21989b72ffaf2916135127ee5
beguganjangjr <64639270+beguganjangjr@users.noreply.github.com>
2021-06-27 15:56:37 +0700

    Update restart.py

--- bot/handlers/restart.py
+++ bot/handlers/restart.py
@@ -11,24 +11,12 @@ from bot.handlers import fs_utils
 from bot.handlers.fs_utils import *
 
 @Client.on_message(filters.command(COMMAND.RESTART))
-#def restart(client: Client, message: Message):
-    #restart_message = sendMessage(
-    #    "Restarting, Please wait!",
-    #    client,
-    #    message
-    #)
-#    restart_message = message.reply_text("Restarting, Please wait!")
-#    # Save restart message object in order to reply to it after restarting
-##    fs_utils.clean_all()
- #   with open('restart.pickle', 'wb') as status:
- #       pickle.dump(restart_message, status)
- #       status.close()
- #   execl(executable, executable, "-m", "bot")
-def restart(client: Client, message: Message):
-    restart_message = sendMessage("Restarting, Please wait!", client, message)
+async def restart(client : Client, message: Message):
+    restart_message = client.send_message(chat_id=message.chat.id, reply_to_message_id=message.message_id, text="Restarting, Please wait!")
     # Save restart message object in order to reply to it after restarting
     with open(".restartmsg", "w") as f:
         f.truncate(0)
         f.write(f"{restart_message.chat.id}\n{restart_message.message_id}\n")
-    fs_utils.clean_all()
+    await fs_utils.clean_all()
     os.execl(executable, executable, "-m", "bot")
+
